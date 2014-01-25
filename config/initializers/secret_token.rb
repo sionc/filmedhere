e6a6c6d166dfe19a433c606072cce889a1eaaaa0
sionc <sion.chaudhuri@gmail.com>
2014-01-25 04:36:19 -0800

    [Setup] First commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Filmedhere::Application.config.secret_key_base = '098aff87449aeb8f01d94116a7b7931e03a74ad3b4288adc094f9434bc30d8c2e14482b81d9f06776437bf8269f5ca3df8ff597481221b32a23ce4b21b00daf3'
