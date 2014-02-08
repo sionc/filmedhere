58b1da876289f23710d2ce50f063f32a57ac280e
sionc <sion.chaudhuri@gmail.com>
2014-02-07 18:03:05 -0800

    [Maps] Show SF map with all filming locations
    - Added locations to index and show actions for films

--- app/controllers/films_controller.rb
+++ app/controllers/films_controller.rb
@@ -5,11 +5,22 @@ class FilmsController < ApplicationController
   # GET /films.json
   def index
     @films = Film.all
+
+    respond_to do |format|
+      format.html # index.html.erb
+      format.json { render json: @films, :include => :locations }
+    end
   end
 
   # GET /films/1
   # GET /films/1.json
   def show
+    @film = Film.find(params[:id])
+
+    respond_to do |format|
+      format.html # show.html.erb
+      format.json { render json: @film, :include => :locations }
+    end
   end
 
   # GET /films/new
