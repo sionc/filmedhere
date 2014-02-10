0024cc8b0ffa0b2a8a1d165a86395443c0525ba1
sionc <sion.chaudhuri@gmail.com>
2014-02-10 03:17:37 -0600

    [Deploy] Enabling all filming locations

--- app/controllers/films_controller.rb
+++ app/controllers/films_controller.rb
@@ -4,7 +4,7 @@ class FilmsController < ApplicationController
   # GET /films
   # GET /films.json
   def index
-    @films = Film.all.limit(5)
+    @films = Film.all
 
     respond_to do |format|
       format.html # index.html.erb
